2624991c9718db45132056012478c342053b9e01
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-27 10:59:40 +0300

    Added background video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
-import styles from "./main.module.scss";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.videoContainer}>
+        <iframe
+          src="https://player.vimeo.com/video/1013433471?autoplay=1&loop=1&autopause=0&muted=1&badge=0&title=0&sidedock=0&controls=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          loading="lazy"
+          className={styles.backgroundVideo}
+          frameBorder="0"
+        />
+      </div>
+    </div>
+  );
 }

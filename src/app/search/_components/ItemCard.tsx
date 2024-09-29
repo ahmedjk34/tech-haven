cd05a0242055aa2cfcfdb34fbd6583f66e807c57
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-29 16:39:00 +0300

    Added item card component & its elements

---
+++ src/app/search/_components/ItemCard.tsx
@@ -0,0 +1,38 @@
+import { ItemType } from "@/util/Types";
+import styles from "../searchPage.module.scss";
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  item: ItemType;
+};
+
+function ItemCard({ item }: Props) {
+  const { name, images, _id, price, discount, subCategories } = item;
+  const showImage = images[0];
+  return (
+    <div className={styles.item}>
+      <div className={styles.imageHolder}>
+        <Image src={showImage} alt={name} loading="lazy" layout="fill" />
+      </div>
+      <div className={styles.itemInfo}>
+        <div className={styles.mainInfo}>
+          <h3>{name}</h3>
+          <p>
+            {discount !== 0
+              ? (price - (price * discount) / 100).toFixed(2) // Subtract discount from price
+              : price.toFixed(2)}
+            $
+          </p>
+        </div>
+        <div className={styles.subCategories}>
+          {subCategories.map((subCategory) => (
+            <div>{subCategory}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCard;

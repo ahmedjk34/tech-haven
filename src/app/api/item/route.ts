354c2a2b7029236b9f615ac8a688e13001b3b6c7
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-29 15:53:15 +0300

    Added items route to query for items at the search page

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -1,35 +1,37 @@
 import connectDB from "@/lib/connectDB";
 import itemModel from "@/models/itemModel";
-import { ItemType } from "@/util/Types";
 import { NextRequest, NextResponse } from "next/server";
 
-const array: ItemType[] = [
-  {
-    name: "AMD Ryzen 7 7800X3D",
-    price: 339,
-    brand: "AMD",
-    category: "CPU",
-    subCategories: ["Enthusiast", "AMD", "Core i7"],
-    specifications: new Map<string, string>([
-      ["core_count", "8"],
-      ["core_clock", "4.2"],
-      ["boost_clock", "5"],
-      ["tdp", "120"],
-      ["graphics", "Radeon"],
-      ["smt", "true"],
-    ]),
-    images: [
-      "https://c1.neweggimages.com/ProductImageCompressAll1280/19-113-793-03.png",
-      "https://c1.neweggimages.com/ProductImageCompressAll1280/19-113-793-02.png",
-      "https://c1.neweggimages.com/ProductImageCompressAll1280/19-113-793-01.png",
-    ],
-    stock: 50,
-    discount: 0,
-  },
-];
+export async function GET(request: NextRequest) {
+  try {
+    await connectDB();
+    const searchParams = request.nextUrl.searchParams;
+    const category = searchParams.get("category");
+    const subCategories = searchParams.getAll("subCategories")[0].split(",");
+    const brand = searchParams.get("brand");
+    console.log(category, brand, subCategories);
+    const query: any = {};
 
-export async function POST(request: NextRequest) {
-  // await connectDB();
-  // array.forEach(async (item) => await itemModel.create(item));
-  // return new NextResponse("Success");
+    if (category && category !== "undefined") {
+      query.category = category;
+    }
+
+    if (subCategories && subCategories[0] !== "undefined") {
+      query.subCategories = { $in: subCategories };
+    }
+
+    if (brand && brand !== "undefined") {
+      query.brand = brand;
+    }
+
+    // Fetch items based on the constructed query
+    const items = await itemModel.find(query);
+
+    return NextResponse.json(items);
+  } catch (error) {
+    return NextResponse.json(
+      { error: `An error occurred: ${error.message}` },
+      { status: 500 }
+    );
+  }
 }

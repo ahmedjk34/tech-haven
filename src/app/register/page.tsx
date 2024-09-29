33d31232b2df28883a745b75eee37bd2c1e37249
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-29 23:09:58 +0300

    Added Sign Up form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../../styles/pages/authenticationPage.module.scss";
+import AuthenticationForm from "@/components/AuthenticationForm/AuthenticationForm";
+type Props = {};
+
+function page({}: Props) {
+  return (
+    <div className={styles.authPage}>
+      <AuthenticationForm
+        type="Sign Up"
+        fields={["name", "email", "password"]}
+      />
+    </div>
+  );
+}
+
+export default page;

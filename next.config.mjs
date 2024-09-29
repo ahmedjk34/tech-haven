575676e69e9376c0cc3ff2c46876ba03bec9554c
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-29 16:38:27 +0300

    Added a source for images in the nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["c1.neweggimages.com"], // Add other domains as needed
+  },
+};
 
 export default nextConfig;

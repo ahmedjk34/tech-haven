a0d66f51a408c096349ece3aef91d24970bfca83
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-29 15:53:37 +0300

    Added items holder & worked on the API compatibility

---
+++ src/app/search/_components/ItemsHolder.tsx
@@ -0,0 +1,36 @@
+"use server";
+import React from "react";
+import styles from "../searchPage.module.scss";
+import axios from "axios";
+import { ItemType } from "@/util/Types";
+
+type Props = {};
+
+export async function ItemsHolder({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const response = await axios.get(
+    `http://localhost:3000/api/item?category=${searchParams?.category}&subCategories=${searchParams?.subCategories}&brand=${searchParams?.brand}`
+  );
+  const items: ItemType[] = response.data;
+  //console.log(items);
+
+  if (response.status == 200)
+    return (
+      <div className={styles.itemsHolder}>
+        {items?.map((item) => (
+          <div>{item.brand}</div>
+        ))}
+      </div>
+    );
+  else
+    return (
+      <div>
+        <h2>An error fetching data occurred</h2>
+      </div>
+    );
+}
+
+export default ItemsHolder;
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import styles from "./searchPage.module.scss";
 import CategorySelector from "./_components/CategorySelector";
 import SideBar from "./_components/SideBar";
+import ItemsHolder from "./_components/ItemsHolder";
 
-type Props = {};
+type Props = {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
 
-function page({}: Props) {
+function page({ params, searchParams }: Props) {
   return (
     <div className={styles.searchPage}>
       <CategorySelector />
       <SideBar />
+      <ItemsHolder searchParams={searchParams} />
     </div>
   );
 }

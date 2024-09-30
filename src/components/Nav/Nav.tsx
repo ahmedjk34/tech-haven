e48b719c1658b8c3e41a94e8413ddc7925523ab3
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-30 18:25:55 +0300

    Extracted the search input into it's own component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,29 +1,21 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import styles from "./nav.module.scss";
 import logo from "../../../public/logo.png";
 import Image from "next/image";
 import { CgShoppingCart, CgProfile } from "react-icons/cg";
 import { useRouter } from "next/navigation";
+import InputHolder from "./InputHolder";
 
 type Props = {};
 
 function Nav({}: Props) {
-  const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
 
   return (
     <div className={styles.nav}>
-      <Image src={logo} alt="logo" onClick={(e) => router.push("/")} />
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleSearchChange}
-        placeholder="Search..."
-      />
+      <Image src={logo} alt="logo" onClick={() => router.push("/")} />
+      <InputHolder />
       <div className={styles.iconsHolder}>
         <CgShoppingCart size={30} className={styles.icon} />
         <h2>|</h2>

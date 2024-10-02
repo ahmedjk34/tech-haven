6e968697bb1ee366b475f7e34600037c56ab1ad2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-02 20:57:36 +0300

    Created the addToCart function

---
+++ src/util/cartActions.ts
@@ -0,0 +1,24 @@
+import { ItemType, ContextType } from "@/util/Types";
+
+// Add item to the cart
+export function addItemToCart(
+  cart: ContextType[],
+  setCart: React.Dispatch<React.SetStateAction<ContextType[]>>,
+  item: ItemType
+) {
+  setCart((prevCart) => {
+    const existingItemIndex = prevCart.findIndex(
+      (cartItem) => cartItem.data._id === item._id
+    );
+
+    if (existingItemIndex !== -1) {
+      // If the item already exists in the cart, increment the quantity
+      const updatedCart = [...prevCart];
+      updatedCart[existingItemIndex].quantity += 1;
+      return updatedCart;
+    } else {
+      // If the item does not exist, add it to the cart with quantity 1
+      return [...prevCart, { data: item, quantity: 1 }];
+    }
+  });
+}

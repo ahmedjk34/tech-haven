d88d632ad3a6c231e3e5cffbe499a1ea8eef90af
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-02 20:20:16 +0300

    Added CartWindow to the nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,27 +1,36 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./nav.module.scss";
 import logo from "../../../public/logo.png";
 import Image from "next/image";
 import { CgShoppingCart, CgProfile } from "react-icons/cg";
 import { useRouter } from "next/navigation";
 import InputHolder from "./InputHolder";
+import CartWindow from "./CartWindow";
 
 type Props = {};
 
 function Nav({}: Props) {
   const router = useRouter();
+  const [isCartActive, setIsCartActive] = useState(false);
 
   return (
-    <div className={styles.nav}>
-      <Image src={logo} alt="logo" onClick={() => router.push("/")} />
-      <InputHolder />
-      <div className={styles.iconsHolder}>
-        <CgShoppingCart size={30} className={styles.icon} />
-        <h2>|</h2>
-        <CgProfile size={30} className={styles.icon} />
+    <>
+      <div className={styles.nav}>
+        <Image src={logo} alt="logo" onClick={() => router.push("/")} />
+        <InputHolder />
+        <div className={styles.iconsHolder}>
+          <CgShoppingCart
+            size={30}
+            className={styles.icon}
+            onClick={() => setIsCartActive((prev) => !prev)}
+          />
+          <h2>|</h2>
+          <CgProfile size={30} className={styles.icon} />
+        </div>
       </div>
-    </div>
+      <CartWindow active={isCartActive} toggleActivity={setIsCartActive} />
+    </>
   );
 }
 

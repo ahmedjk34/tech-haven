0d425a7cf8184096dd97451c979cfbfba86af184
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-03 15:05:36 +0300

    Finished most of the cart window & it's functionality

--- src/components/Nav/CartWindow.tsx
+++ src/components/Nav/CartWindow.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import styles from "./nav.module.scss";
 import { useCart } from "../CartProvider/CartProvider";
 import { ContextType } from "../../util/Types";
+import {
+  calculateTotalPrice,
+  formatPriceAfterDiscount,
+} from "@/util/priceUtil";
+import { addItemToCart, deleteItemFromCart } from "@/util/cartActions";
 
 type Props = {
   active: boolean;
@@ -21,15 +26,59 @@ function CartWindow({ active, toggleActivity }: Props) {
         >
           Close The Cart
         </h2>
-        <div>
+        <div className={styles.cartItemsHolder}>
           {cart.length > 0 ? (
             cart.map((item: ContextType) => (
-              <div key={item.data._id?.toString()}>
-                {item.data.name} - Quantity: {item.quantity}
+              <div key={item.data._id?.toString()} className={styles.cartItem}>
+                <div className={styles.imageHolder}>
+                  <img src={item.data.images[0]} />
+                </div>
+                <div className={styles.cartItemInfo}>
+                  <h3>{item.data.name}</h3>
+                  <div>
+                    <h4
+                      className={styles.quantityControl}
+                      onClick={() => addItemToCart(cart, setCart, item.data)}
+                    >
+                      +
+                    </h4>
+                    <h3>Quantity: {item.quantity}</h3>
+                    <h4
+                      className={styles.quantityControl}
+                      onClick={() =>
+                        deleteItemFromCart(cart, setCart, item.data)
+                      }
+                    >
+                      -
+                    </h4>
+                  </div>
+                  <div>
+                    <span>
+                      Total Price: {item.quantity} X{" "}
+                      {formatPriceAfterDiscount(
+                        item.data.price,
+                        item.data.discount
+                      )}
+                      $:
+                    </span>
+                    <h4 style={{ color: "green" }}>
+                      {calculateTotalPrice(
+                        Number(
+                          formatPriceAfterDiscount(
+                            item.data.price,
+                            item.data.discount
+                          )
+                        ),
+                        item.quantity
+                      )}
+                      $
+                    </h4>
+                  </div>
+                </div>
               </div>
             ))
           ) : (
-            <p>Your cart is empty.</p>
+            <h1 className={styles.emptyCartMessage}>Your cart is empty.</h1>
           )}
         </div>
       </div>

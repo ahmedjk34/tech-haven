ae30554f7df312b62adec268c64ba06692fa3603
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-04 14:49:57 +0300

    Added the route to add to the wishlist

---
+++ src/app/api/user/[userId]/wishlist/[itemId]/route.ts
@@ -0,0 +1,28 @@
+import userModel from "@/models/userModel";
+import mongoose, { Mongoose } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { userId: string; itemId: string } }
+) {
+  const { userId, itemId } = params;
+  try {
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) &&
+      !mongoose.Types.ObjectId.isValid(itemId)
+    )
+      throw new Error("Not Valid MongoDB Object ID");
+
+    await userModel.findByIdAndUpdate(userId, {
+      $addToSet: { wishlist: itemId },
+    });
+    return NextResponse.json({
+      message: `Success`,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: `User ID: ${userId}, Item ID: ${itemId}`,
+    });
+  }
+}

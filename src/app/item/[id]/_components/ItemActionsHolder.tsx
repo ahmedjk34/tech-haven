fde74cf409ac55802ac8fa4da0997b6fccf01fd7
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-05 12:47:09 +0300

    Made the changes to wishlist actually update the user session

--- src/app/item/[id]/_components/ItemActionsHolder.tsx
+++ src/app/item/[id]/_components/ItemActionsHolder.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { ItemType } from "@/util/Types";
 import { useCart } from "@/components/CartProvider/CartProvider";
 import { addItemToCart } from "@/util/cartActions";
 import axios from "axios";
-import { Session } from "next-auth";
 import { getSession, useSession } from "next-auth/react";
 
 type Props = {
@@ -17,19 +16,25 @@ function ItemActionsHolder({ item }: Props) {
     addItemToCart(cart, setCart, item);
   };
   const { data: session, update } = useSession();
-
   const handleAddToWishList = async () => {
     try {
       if (session?.user) {
         await axios.post(
           `http://localhost:3000/api/user/${session.user.id}/wishlist/${item._id}`
         );
-        // await update({
-        //   ...session,
-        //   user: {
-        //     ...session?.user,
-        //   },
-        // });
+        const itemExists = session.user.wishlist.some(
+          (existingItem) => existingItem._id === item._id
+        );
+        if (!itemExists) {
+          await update({
+            ...session,
+            user: {
+              ...session.user,
+              wishlist: [...session.user.wishlist, item],
+            },
+          });
+          window.location.reload();
+        }
         alert(`${item.name} added to wishlist!`);
       } else {
         alert("You need to be logged in to add to wishlist.");
@@ -45,7 +50,9 @@ function ItemActionsHolder({ item }: Props) {
       <h3>{item.stock} Available in stock</h3>
       <button onClick={handleAddToCart}>Add {item.name} to cart +</button>
       {session?.user && (
-        <div onClick={handleAddToWishList}>Add to wishlist +</div>
+        <div onClick={async () => await handleAddToWishList()}>
+          Add to wishlist +
+        </div>
       )}
     </div>
   );

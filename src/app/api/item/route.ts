464490f3d4cd1193af2acd2b7dd4c63b0719eb64
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-05 21:37:26 +0300

    Added filtering based on min and max price

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -1,21 +1,27 @@
 import connectDB from "@/lib/connectDB";
 import itemModel from "@/models/itemModel";
 import { NextRequest, NextResponse } from "next/server";
+import { formatPriceAfterDiscount } from "@/util/priceUtil"; // Import the price calculation function
 
 export async function GET(request: NextRequest) {
   try {
     await connectDB();
+
     const searchParams = request.nextUrl.searchParams;
     const category = searchParams.get("category");
-    const subCategories = searchParams.getAll("subCategories")[0].split(",");
+    const subCategories =
+      searchParams.getAll("subCategories")[0]?.split(",") || [];
     const brand = searchParams.get("brand");
+    const minPrice = parseFloat(searchParams.get("minPrice") || "0");
+    const maxPrice = parseFloat(searchParams.get("maxPrice") || "Infinity");
+
     const query: any = {};
 
     if (category && category !== "undefined") {
       query.category = category;
     }
 
-    if (subCategories && subCategories[0] !== "undefined") {
+    if (subCategories.length && subCategories[0] !== "undefined") {
       query.subCategories = { $in: subCategories };
     }
 
@@ -23,9 +29,17 @@ export async function GET(request: NextRequest) {
       query.brand = brand;
     }
 
+    // Fetch items based on query
     const items = await itemModel.find(query);
 
-    return NextResponse.json(items);
+    // Filter items based on price after discount
+    const filteredItems = items.filter((item) => {
+      const priceAfterDiscount = parseFloat(
+        formatPriceAfterDiscount(item.price, item.discount)
+      );
+      return priceAfterDiscount >= minPrice && priceAfterDiscount <= maxPrice;
+    });
+    return NextResponse.json(filteredItems);
   } catch (error: unknown) {
     return NextResponse.json(
       { error: `An error occurred: ${error}` },

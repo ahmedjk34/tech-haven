bcec8423052291621ef6c158bb46eac9bf9a8a50
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-05 14:34:12 +0300

    added extractTotalPriceFromCart function

--- src/util/priceUtil.ts
+++ src/util/priceUtil.ts
@@ -1,3 +1,5 @@
+import { ContextType } from "@/util/Types";
+
 export function formatPriceAfterDiscount(
   price: number,
   discount: number
@@ -8,4 +10,16 @@ export function formatPriceAfterDiscount(
 
 export function calculateTotalPrice(price: number, quantity: number): string {
   return (price * quantity).toFixed(2);
+}
+
+export function extractTotalPriceFromCart(cart: ContextType[]): number {
+  return cart.reduce((accumulator: number, current: ContextType) => {
+    const priceAfterDiscount = Number(
+      formatPriceAfterDiscount(current.data.price, current.data.price)
+    );
+    const totalPrice = Number(
+      calculateTotalPrice(priceAfterDiscount, current.quantity)
+    );
+    return accumulator + totalPrice;
+  }, 0);
 }

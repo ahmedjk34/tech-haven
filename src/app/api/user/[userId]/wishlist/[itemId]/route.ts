eb4971203e1f1febeacb88129a37ccbca9643e37
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-05 13:08:20 +0300

    Added DELETE handler for removing from the wishlist

--- src/app/api/user/[userId]/wishlist/[itemId]/route.ts
+++ src/app/api/user/[userId]/wishlist/[itemId]/route.ts
@@ -26,3 +26,28 @@ export async function POST(
     });
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { userId: string; itemId: string } }
+) {
+  const { userId, itemId } = params;
+  try {
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(itemId)
+    )
+      throw new Error("Not Valid MongoDB Object ID");
+
+    await userModel.findByIdAndUpdate(userId, {
+      $pull: { wishlist: itemId },
+    });
+    return NextResponse.json({
+      message: `Success`,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: `User ID: ${userId}, Item ID: ${itemId}`,
+    });
+  }
+}

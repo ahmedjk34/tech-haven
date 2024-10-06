1cb042fc7fa35255ab1961bd1909470293941340
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-06 10:26:42 +0300

    Added optimization (when the item is in the wishlist just return)

--- src/app/api/user/[userId]/wishlist/[itemId]/route.ts
+++ src/app/api/user/[userId]/wishlist/[itemId]/route.ts
@@ -7,22 +7,39 @@ export async function POST(
   { params }: { params: { userId: string; itemId: string } }
 ) {
   const { userId, itemId } = params;
+
   try {
     if (
       !mongoose.Types.ObjectId.isValid(userId) ||
       !mongoose.Types.ObjectId.isValid(itemId)
-    )
+    ) {
       throw new Error("Not Valid MongoDB Object ID");
+    }
+
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const alreadyInWishlist = user.wishlist.includes(itemId);
+
+    if (alreadyInWishlist) {
+      return NextResponse.json({
+        message: "Item is already in the wishlist",
+      });
+    }
 
     await userModel.findByIdAndUpdate(userId, {
       $addToSet: { wishlist: itemId },
     });
+
     return NextResponse.json({
-      message: `Success`,
+      message: "Item added to wishlist successfully",
     });
   } catch (error) {
     return NextResponse.json({
-      message: `User ID: ${userId}, Item ID: ${itemId}`,
+      message: `Error: ${error}`,
     });
   }
 }
